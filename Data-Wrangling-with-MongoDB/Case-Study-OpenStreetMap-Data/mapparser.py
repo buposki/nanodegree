338f0e7a63ee4603ccbe7bbeb6d103acc31cfa7e
vverde <amodiovalerio.verde@gmail.com>
2016-09-03 14:00:18 +0200

    Update mapparser.py
    Some cleaning

--- Data-Wrangling-with-MongoDB/Case-Study-OpenStreetMap-Data/mapparser.py
+++ Data-Wrangling-with-MongoDB/Case-Study-OpenStreetMap-Data/mapparser.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
 # mapparser.py
 # Data Wrangling with MongoDB (Udacity.com)
 # Project 3: OpenStreetMap Data Case Study
@@ -14,20 +11,15 @@ from collections import defaultdict
 def count_tags(filename):
     # We use defaultdict so that we don't need to check if an element with same tag already exists
     tag_count = defaultdict(int)
-
     # Iter along elements and increment tag count
     for _, element in ET.iterparse(filename):
-        
         # Increment tag count by 1 - As we initialize tag_count with defaultdict, tag doesn't need to be already in dictionary
         tag_count[element.tag] += 1
-
         # Clear element from memory. It (really) speeds up execution and avoid computer locking due low memory
         element.clear()
-
     return tag_count
 
 def test():
-
     tags = count_tags('example.osm')
     pprint.pprint(tags)
     assert tags == {'bounds': 1,
